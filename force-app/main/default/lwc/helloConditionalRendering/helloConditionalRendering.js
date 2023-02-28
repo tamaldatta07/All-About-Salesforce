5e035d0d57f97142dc7b9b8aea742671ab178574
Tamal Datta <77050253+tamaldatta07@users.noreply.github.com>
2023-02-28 13:01:11 +0530

    updated

--- force-app/main/default/lwc/helloConditionalRendering/helloConditionalRendering.js
+++ force-app/main/default/lwc/helloConditionalRendering/helloConditionalRendering.js
@@ -2,7 +2,14 @@ import { LightningElement } from 'lwc';
 
 export default class HelloConditionalRendering extends LightningElement {
     isVisible = false
+    name
     handleClick(){
         this.isVisible = true
+    }
+    changeHandler(){
+        this.name = event.target.value
+    }
+    get helloMethod(){
+        return this.name ==='Hello'
     }
 }
